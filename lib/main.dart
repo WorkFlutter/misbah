0d951e4e73a5e2809883d2e784ff28fa7f64689b
Mohammed <asdasdm94@gmail.com>
2021-03-18 12:40:16 +0200

    finish app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:misbah/screens/1home.dart';
 
 void main() {
   runApp(MyApp());
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark));
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: Size(360, 760),
-      allowFontScaling: false,
-      builder: () => GetMaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'مسباح',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-
+        title: 'مسباح',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
+        home: HomePage(),
       ),
     );
   }
 }
-
--- lib/screens/1home.dart
+++ lib/screens/1home.dart
@@ -1,11 +1,63 @@
-
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:misbah/widgets/0app_bar.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: MyAppBar(
+        widgets: [
+          SizedBox(
+            width: 18,
+          ),
+          Image.asset('assets/icons/add.png'),
+          SizedBox(
+            width: 5,
+          ),
+          SizedBox(
+            width: 245 / 360 * size.width,
+            height: 40 / 760 * size.height,
+            child: Directionality(
+              textDirection: TextDirection.rtl,
+              child: TextField(
+                cursorColor: Colors.black,
+                cursorHeight: 16,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    CupertinoIcons.search,
+                    size: 24,
+                  ),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 2, horizontal: 12),
+                  hintText: 'البحث',
+                  hintStyle: TextStyle(fontSize: 12),
+                  fillColor: Color(0xffF1F1F1),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 14,
+          ),
+          GestureDetector(
+            child: Icon(
+              Icons.menu,
+              size: 30,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Hello'),
+      ),
+    );
   }
 }
